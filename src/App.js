6309bf1ff2b371fdc1a73a800fb41e4345784d1e
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-11-30 12:45:53 -0800

    a few small style changes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Main from './components/Main/Main'
 
+import background from './stars.gif'
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${background})` }}>
       <Header />
       <Footer />
       <Main />
