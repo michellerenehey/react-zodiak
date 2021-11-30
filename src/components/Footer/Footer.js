44b79943ff21c0ad47f64232cc32052ece0e7c0b
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-11-30 12:07:15 -0800

    added link to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Footer.css'
 export default function Footer() {
-  return <footer>Made by Michelle Nygren</footer>
+  return (
+    <footer>
+      Made by <a href="https://www.linkedin.com/in/michellenygren/"> Michelle Nygren</a>
+    </footer>
+  )
 }
