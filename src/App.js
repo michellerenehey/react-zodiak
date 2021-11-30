ea5f9b80b9eefaf1eb92410ed287eede983b7496
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-11-29 18:00:21 -0800

    working header

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react'
 import './App.css'
 
 import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Main from './components/Main/Main'
+// import Footer from './components/Footer/Footer'
+// import Main from './components/Main/Main'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Footer />
-      <Main />
     </div>
   )
 }
