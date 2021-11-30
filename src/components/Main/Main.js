25ab0c7c6d73a8f1e330c5140dfd02845f6d928e
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-11-30 12:23:38 -0800

    added gif as background of main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,10 +2,11 @@ import React from 'react'
 import './Main.css'
 import ZodiacCard from '../ZodiacCard/ZodiacCard'
 import { zodiac } from '../../data'
+import background from '../../stars.gif'
 
 export default function Main() {
   return (
-    <main>
+    <main style={{ backgroundImage: `url(${background})` }}>
       {zodiac.map((sign) => (
         <ZodiacCard key={sign.name} name={sign.name} dates={sign.dates} />
       ))}
