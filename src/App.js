e1de4c887157203ce680a343744f52f26348a918
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-11-29 18:03:56 -0800

    working footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import './App.css'
 
 import Header from './components/Header/Header'
-// import Footer from './components/Footer/Footer'
+import Footer from './components/Footer/Footer'
 // import Main from './components/Main/Main'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   )
 }
