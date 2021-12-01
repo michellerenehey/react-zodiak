1698aaf10a63b598ac079384b48d47050c938d6b
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-11-30 16:33:18 -0800

    updated hover of zodiac cards

--- src/components/ZodiacCard/ZodiacCard.js
+++ src/components/ZodiacCard/ZodiacCard.js
@@ -1,6 +1,14 @@
 import './ZodiacCard.css'
 
 export default function ZodiacCard({ name, dates }) {
+  // function redBackground(e) {
+  //   e.target.style.background = 'red'
+  // }
+
+  // function greenBackground(e) {
+  //   e.target.style.background = '#7fb285'
+  // }
+
   return (
     <div className="zodiacCard">
       <img src={`${process.env.PUBLIC_URL}/images/${name}.png`} />
